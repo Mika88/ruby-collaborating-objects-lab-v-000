4700eba47f9c1fa2642ec4163ba2b21835a785f0
Mika88 <mikaspivak@gmail.com>
2018-08-08 16:19:51 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,4 @@ class Song
     song.name = filename.split(" - ")[1]
     song
   end
-  def self.new_by_filename
-    song = self.new
-    song.name = filename.split(" - ")[1]
-    song
-  end
 end
