69e4f0a13064647b5b46dae048ca8fe52be090e5
Mika88 <mikaspivak@gmail.com>
2018-08-08 17:23:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,11 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :songs
   @@all = []
   def initialize(name)
     @name = name
     @songs = []
   end
-
-  def songs
-    @songs
-  end
-
+  
   def add_song(song)
     @songs << song
   end
